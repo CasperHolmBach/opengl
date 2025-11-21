5532ae5f0f1a4fce6dc506c5fa723f65b5594456
Casper Holm Bach <Casper-hb@hotmail.dk>
2025-11-21 16:49:31 +0100

    hygge

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
+// Forward declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
 
 int main()
 {
@@ -23,21 +23,37 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
-
+    
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD";
         return -1;
     }
-
-    glViewport(0,0,800,600);
+    
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Verticy data
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    // Render loop
     while(!glfwWindowShouldClose(window))
     {
-        //Input
+        // Check for input
+        processInput(window);
 
-        //Rendering
+        // Render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -45,4 +61,17 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
